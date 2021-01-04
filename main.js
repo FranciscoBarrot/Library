db614d621e3020bd5804ddef8c010b276e317f6f
Barrot Francisco <franciscobarrot98@gmail.com>
2021-01-04 16:18:41 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+let myLibrary =
+
+
+
+function Book (title, author,pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function(){
+        let str
+        if (read){
+            str = " already read"
+        }
+        else{
+            str = " not read yet"
+        }
+        return title + " by " + author + ", " + pages + " pages," + str
+    }
+}
+
+
+Book.prototype.getPages = function(){
+    return this.pages
+}
+
+const book1 = new Book("Brave New World" , "Aldous Huxley" , 200 , false)
+
+console.log(book1) 
