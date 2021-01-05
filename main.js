1b1ca99fbffeb185eaf50fd143011facae5a3c26
Barrot Francisco <franciscobarrot98@gmail.com>
2021-01-04 21:11:13 -0300

    new book button added

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 let myLibrary = []
 
-
 /* CONSTRUCTOR */
-function Book (title, author,pages, read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function(){
-        let str
-        if (read){
-            str = " already read"
-        }
-        else{
-            str = " not read yet"
-        }
-        return title + " by " + author + ", " + pages + " pages," + str
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = function () {
+    let str
+    if (read) {
+      str = ' already read'
+    } else {
+      str = ' not read yet'
     }
+    return title + ' by ' + author + ', ' + pages + ' pages,' + str
+  }
 }
 
-
-Book.prototype.getPages = function(){
-    return this.pages
+Book.prototype.getPages = function () {
+  return this.pages
 }
 
-const book1 = new Book("Brave New World" , "Aldous Huxley" , 200 , false)
+const book1 = new Book('Brave New World', 'Aldous Huxley', 200, false)
 
-console.log(book1) 
-
+console.log(book1)
 
 /* FUNCTIONS */
-function addBookToLibrary (){
-    const oneBook = new Book()
-    myLibrary.push("hol<")
+function addBookToLibrary(title, author, pages, readed) {
+  const oneBook = new Book(title, author, pages, readed)
+  myLibrary.push(oneBook)
+}
+
+function display() {
+  myLibrary.forEach()
 }
