e0dc8f06460eca8ab7b8e4e7dbfa72babd3189d6
Barrot Francisco <franciscobarrot98@gmail.com>
2021-01-06 19:45:10 -0300

    form style incomming

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let myLibrary = []
 const addBtn = document.querySelector('.addBookBtn')
 const submitBtn = document.querySelector('#submit-btn')
+const cardsContainer = document.querySelector('.display-container')
 
 addBtn.addEventListener('click', () => {
   document.querySelector('.popup').classList.toggle('show')
@@ -27,13 +28,15 @@ function Book(title, author, pages, read) {
   }
 }
 
-Book.prototype.getPages = function () {
-  return this.pages
+Book.prototype.info = function () {
+  let str
+  if (read) {
+    str = ' Already read'
+  } else {
+    str = ' Not read yet'
+  }
+  return title + ' by ' + author + ', ' + pages + ' pages,' + str
 }
-
-const book1 = new Book('Brave New World', 'Aldous Huxley', 200, false)
-
-console.log(book1)
 
 /* FUNCTIONS */
 function addCliked(e) {}
@@ -46,3 +49,31 @@ function addBookToLibrary(title, author, pages, readed) {
 function display() {
   myLibrary.forEach()
 }
+
+/* displays */
+function displayCards() {
+  myLibrary.forEach((book) => {
+    const card = document.createElement('div')
+    const title = document.createElement('h2')
+    const author = document.createElement('h2')
+    const pages = document.createElement('h2')
+    const readed = document.createElement('h2')
+
+    title.textContent = book.title
+    author.textContent = book.author
+    pages.textContent = book.pages
+    readed.textContent = book.read
+
+    card.appendChild(title)
+    card.appendChild(author)
+    card.appendChild(pages)
+    card.appendChild(readed)
+
+    card.classList.add('card')
+    cardsContainer.appendChild(card)
+  })
+}
+addBookToLibrary('hola', 'german', 1000, true)
+addBookToLibrary('hola', 'german', 1000, true)
+
+displayCards()
